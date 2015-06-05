7962dd08d26845959ddd81d3368842b96674ae08
eastflag <eastflag@gmail.com>
2015-06-05 22:03:36 +0900

    db 추가

--- smartschool/doc/create.sql
+++ smartschool/doc/create.sql
@@ -3,6 +3,18 @@ CREATE TABLE `home` (
   PRIMARY KEY (`home_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+CREATE TABLE `location` (
+  `location_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `member_id` bigint(20) NOT NULL,
+  `lat` double NOT NULL,
+  `lng` double NOT NULL,
+  `address` varchar(450) COLLATE utf8_bin DEFAULT NULL,
+  `created_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`location_id`),
+  KEY `FK_LOCATION_MEMBER` (`member_id`),
+  CONSTRAINT `FK_LOCATION_MEMBER` FOREIGN KEY (`member_id`) REFERENCES `member` (`member_id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
 CREATE TABLE `member` (
   `member_id` bigint(20) NOT NULL AUTO_INCREMENT,
   `home_id` varchar(45) NOT NULL,
