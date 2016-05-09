eff7fefe4527ad31e4db8be99c2016de6135aa98
eastflag <eastflag@gmail.com>
2016-05-09 13:49:52 +0900

    결재 정보 수정에 따른  VO 수정

--- smartschool/src/main/java/com/aura/smartschool/domain/PayVO.java
+++ smartschool/src/main/java/com/aura/smartschool/domain/PayVO.java
@@ -1,26 +1,12 @@
 package com.aura.smartschool.domain;
 
+import lombok.Data;
+
+@Data
 public class PayVO {
 	private int pay_id;
 	private int member_id;
 	private String pay_date;
-
-	public int getPay_id() {
-		return pay_id;
-	}
-	public void setPay_id(int pay_id) {
-		this.pay_id = pay_id;
-	}
-	public int getMember_id() {
-		return member_id;
-	}
-	public void setMember_id(int member_id) {
-		this.member_id = member_id;
-	}
-	public String getPay_date() {
-		return pay_date;
-	}
-	public void setPay_date(String pay_date) {
-		this.pay_date = pay_date;
-	}
+	private String mxIssueNo;
+	private int goods_id;
 }
